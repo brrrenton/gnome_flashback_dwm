de18f0257dab45118bbe3876d98828cad6794a36
Brenton Leighton <brenton.leighton@uts.edu.au>
2020-06-05 23:11:19 +1000

    Use Python status script

--- dwm.sh
+++ dwm.sh
@@ -4,8 +4,8 @@ test -n "$DESKTOP_AUTOSTART_ID" && {
 	dbus-send --print-reply --session --dest=org.gnome.SessionManager "/org/gnome/SessionManager" org.gnome.SessionManager.RegisterClient "string:gnome-flashback-dwm" "string:$DESKTOP_AUTOSTART_ID"
 }
 
-xsetroot -solid "#444444" &
-slstatus &
+hsetroot -solid "#444444" &
+status.py &
 dwm
 
 test -n "$DESKTOP_AUTOSTART_ID" && {
