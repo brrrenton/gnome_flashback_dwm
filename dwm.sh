77d870ef85046e4125ca7b7941d9864fee443137
Brenton Leighton <brenton.leighton@gmail.com>
2020-08-09 17:18:30 +1000

    Run xrdb in dwm.sh

--- dwm.sh
+++ dwm.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-test -n "$DESKTOP_AUTOSTART_ID" && {
+if [ -n "$DESKTOP_AUTOSTART_ID" ]; then
 	dbus-send --print-reply --session --dest=org.gnome.SessionManager "/org/gnome/SessionManager" org.gnome.SessionManager.RegisterClient "string:gnome-flashback-dwm" "string:$DESKTOP_AUTOSTART_ID"
-}
+fi
 
+xrdb ~/.Xresources
 hsetroot -solid "#444444" &
 status.py &
 dwm
 
-test -n "$DESKTOP_AUTOSTART_ID" && {
+if [ -n "$DESKTOP_AUTOSTART_ID" ]; then
     dbus-send --print-reply --session --dest=org.gnome.SessionManager "/org/gnome/SessionManager" org.gnome.SessionManager.Logout "uint32:1"
-}
+fi
 
