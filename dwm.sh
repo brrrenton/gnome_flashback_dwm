533cd4231ccb015859cd26bf022a3a6de045b6b7
Brenton Leighton <brenton.leighton@gmail.com>
2020-08-12 21:48:45 +1000

    numlockx

--- dwm.sh
+++ dwm.sh
@@ -4,8 +4,9 @@ if [ -n "$DESKTOP_AUTOSTART_ID" ]; then
 	dbus-send --print-reply --session --dest=org.gnome.SessionManager "/org/gnome/SessionManager" org.gnome.SessionManager.RegisterClient "string:gnome-flashback-dwm" "string:$DESKTOP_AUTOSTART_ID"
 fi
 
-xrdb ~/.Xresources
 hsetroot -solid "#444444" &
+xrdb ~/.Xresources &
+numlockx &
 wmname LG3D &
 status.py &
 STATUS_PID=$!
